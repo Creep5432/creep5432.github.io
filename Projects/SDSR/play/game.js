c9434f6043e549d9580d7a43ca6ac675d40aa70a
Creep5432 <creep5432@icloud.com>
2025-05-18 18:07:04 -0500

    SDS Remote Update
    Optimized packet data
    Got rid of unused code
    Made joystick flicks work

--- Projects/SDSR/play/game.js
+++ Projects/SDSR/play/game.js
@@ -4,9 +4,7 @@ window.onerror = (error, url, line) => {
     const text = new Text(error.toString(), -400, 240, 16, "red", "left")
     text.bold = true;
 
-    text.tick = () => {
-        text.y += -1;
-    }
+    text.tick = () => {text.y += -1;}
 
     uiCanvasItems.push(text);
 }
@@ -32,7 +30,14 @@ joy.stickColor = "white";
 joy.baseSize /= 3;
 joy.stickSize /= 3;
 
+joy.onLetGo = () => {}
 joy.tick = () => {
+    if (joy.last !== joy.input) {
+        if (joy.input === { x: 0, y: 0 }) {
+            joy.onLetGo();
+        }
+        joy.last = joy.input;
+    }
     const xAx = (keys["held"]["d"]?1:0) - (keys["held"]["a"]?1:0)
     const yAx = (keys["held"]["s"]?1:0) - (keys["held"]["w"]?1:0)
     if (xAx!==0||yAx!==0) {
@@ -50,40 +55,20 @@ joy.tick = () => {
     }
 
     joy.input.x += xAx/1.25;
-    joy.input.y += yAx/1.25;
-
+    joy.input.y += yAx / 1.25;
 }
 
 const replay = new Button("", 0, 135, 220, 85, "#FFFFFF00");
 replay.bold = true;
 
 const swap = new Button("Right Joystick", 360, -215, 120, 35, "#FFFFFF20");
-swap.onclick = function () {
+swap.onclick = () => {
     swap.text = swap.text=="Left Joystick"?"Right Joystick":"Left Joystick"
-    joy.x *= -1
+    joy.x *= -1;
 }
 canvasItems.push(swap)
 
 var conText = null;
-
-const emotesPopUp = new Button("💬", -400, -210, 50, 50, "black");
-emotesPopUp.toggle = false;
-emotesPopUp.onclick = () => {
-    emotesPopUp.toggle = !emotesPopUp.toggle
-    if (emotesPopUp.toggle) {
-        initEmotes()
-    } else {
-        for (let item of Array.from(canvasItems)) {
-            if ("tag" in item && item.tag === "emote") {
-                const idx = canvasItems.indexOf(item);
-                if (idx >= 0) {
-                    canvasItems.splice(idx, 1);
-                }
-            }
-        }
-    }
-}
-//canvasItems.push(emotesPopUp)
 
 var imgStream = document.createElement("img");
 var imgBuffer = document.createElement("img");
@@ -104,42 +89,9 @@ function changeState(state) {
     if (replayIdx == -1) { uiCanvasItems.push(replay) }
 }
 
-runtime.beforeTick = function () {
+runtime.beforeTick = () => {
     ctx.imageSmoothingEnabled = false;
     ctx.drawImage(stream, 0, 0, canvas.clientWidth, canvas.clientHeight)
-}
-
-
-
-// EMOTES
-function initEmotes () {
-    const emotes = [
-        {abr: "GG!", taunt: "GG!"},
-        {abr: "Supreme!", taunt: "The Supreme Duelist!"},
-        {abr: "Yeah!", taunt: "Yeah!"},
-        {abr: "SD...", taunt: "Self Death..."},
-        {abr: "lol", taunt: "lol"},
-        {abr: "Lag!!!", taunt: "Lag!!!"},
-        {abr: "WTF?!", taunt: "What the fuck?!"},
-        {abr: "Blast you!", taunt: "Blast you!"},
-        {abr: "Good job!", taunt: "Good job!"},
-        {abr: "Thanks!", taunt: "Thanks!"},
-        {abr: "You good?", taunt: "You good?"},
-        {abr: "No.", taunt: "No."},
-        {abr: "Muahaha!", taunt: "Muahaha!"},
-        {abr: "Oopsies.", taunt: "Oopsies."},
-        {abr: "THROWING!", taunt: "STOP THROWING!"},
-        {abr: "Nice.", taunt: "Nice."},
-    ]
-    for (let i = 0; i < emotes.length; i++) {
-        const emoteButton = new Button(emotes[i]["abr"], -320 + (95*Math.floor(i/6)), -225+(((i%6))*25), 90, 20, "#00990075");
-        emoteButton.tag = "emote"
-        emoteButton.onclick = function () {
-            remote.send({celebrate: true, celType: emotes[i]["taunt"]})
-        }
-        canvasItems.push(emoteButton);
-        emotesObjs.push(emoteButton);
-    }
 }
 
 function between(val, min, max) {
@@ -150,11 +102,6 @@ function isWebRTCSupported() {
     return !!(window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection)
 }
 
-
-
-
-
-
 function initConn() {
     uiCanvasItems.splice(uiCanvasItems.indexOf(connect), 1);
     fetch("https://creep5432-sdsr-default-rtdb.firebaseio.com/hostPeer.json").then((res) => {
@@ -163,68 +110,40 @@ function initConn() {
         }
         return res.text()
     }).then((txt) => {
-        console.log(txt);
         const peer = new Peer(null, { debug: 2 });
         peer.on("open", (id) => {
             remote = peer.connect(txt.replaceAll("\"", ""));
             remote.on("open", () => {
+
+                const sendMovePacket = (j) => {
+                    let res = "1";
+                    const ang = angleBetween({ x: 0, y: 0 }, j.input)
+                    const dist = (j.input.x == 0 && j.input.y == 0) ? 0 : clamp(distanceBetween({ x: 0, y: 0 }, j.input), 0.5, 1);
+                    const inp = { x: (Math.cos(ang) * dist), y: Math.sin(ang) * dist }
+
+                    res += String(String.fromCharCode(Math.round(inp.x * 32767) + 32767));
+                    res += String(String.fromCharCode(Math.round(inp.y * -32767) + 32767));
+
+                    remote.send(res);
+                }
+                joy.onLetGo = () => {sendMovePacket(joy.last);}
                 connected = true;
                 changeState("game");
 
-                replay.onclick = function () {
-                    remote.send({ replay: true })
-                }
-
-                setInterval(function () {
-                    const ang = angleBetween({ x: 0, y: 0 }, joy.input)
-                    const dist = (joy.input.x == 0 && joy.input.y == 0) ? 0 : clamp(distanceBetween({ x: 0, y: 0 }, joy.input), 0.5, 1)
-
-                    const inp = {
-                        x: (Math.cos(ang) * dist),
-                        y: Math.sin(ang) * dist
-                    }
-
-                    remote.send({
-                        xJoy: inp.x,
-                        yJoy: inp.y * -1,
-                        id: id
-                    });
-                }, (1000 / 15))
+                replay.onclick = function () {remote.send("2")}
+                setInterval(function () {sendMovePacket(joy)}, (1000 / 20))
 
                 remote.on("data", (data) => {
                     const dat = data;
-                    console.log(dat)
                     if ("slot" in dat) {
                         joy.color = (dat.slot == 1) ? "#fbff0035" : "#0048ff35"
                         joy.stickColor = (dat.slot == 1) ? "#fbff0099" : "#0048ff99"
                     }
-                    if ("celebrate" in dat) {
-                        const cel = new Text(dat["celebrate"], rand(-100, 100), 200, 16, "white", "center");
-                        cel.color = dat["celSlot"] == 0 ? "white" : (dat["celSlot"] == 1 ? "#fbff00" : "#0048ff")
-                        cel.bold = true;
-
-                        cel.tick = function () {
-                            cel.y -= 0.5;
-                            cel.size += 1 / 20
-                        }
-
-                        uiCanvasItems.push(cel);
-
-                        setTimeout(() => {
-                            const idx = uiCanvasItems.indexOf(cel)
-                            if (idx !== -1) {
-                                uiCanvasItems.splice(idx, 1)
-                            }
-                        }, 3500)
-                    }
                 })
 
                 peer.on("call", (call) => {
-                    console.log("Answering call");
                     call.answer();
-
                     call.on("stream", (getStream) => {
-                        console.log("Got stream")
                         if ("srcObject" in stream) {
                             stream.srcObject = getStream
                         } else {
